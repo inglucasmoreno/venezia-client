3081757c0f44ce32e125123c759ca794d0c2d6fd
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2023-02-15 18:44:11 -0300

    Para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  base_url: 'http://157.230.13.158'
+  // base_url: 'http://157.230.13.158'
   // base_url: 'http://178.128.146.6'
-  // base_url: 'https://panaderiavenezia.com'
+  base_url: 'https://panaderiavenezia.com'
 };
