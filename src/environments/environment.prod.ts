646a43bd265ea331aca1b31df6aec45151619d2f
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2024-07-19 20:52:52 -0300

    Produccion Aristobulo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,8 @@ export const environment = {
   // base_url: 'http://157.230.13.158'
   // base_url: 'http://178.128.146.6'
   // base_url: 'http://146.190.52.149',
-  // base_url: 'https://panaderiavenezia.com',
-  base_url: 'https://panaderiavenezialince.com',
+  base_url: 'https://panaderiavenezia.com',
+  // base_url: 'https://panaderiavenezialince.com',
   sucursal: 'SUCURSAL ARISTOBULO'
+  // sucursal: 'SUCURSAL LINCE'
 };
